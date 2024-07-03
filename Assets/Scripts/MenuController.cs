9c39f1434ea0d03def67a7ab1aad4a06866011b6
Tran Hoang Phuc <99522520+thp1923@users.noreply.github.com>
2024-07-03 12:29:48 +0700

    Menu Update

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,23 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Invoke("PlayGame", 1f);
+        }
     }
-    void PlayGame()
+    public void PlayGame()
     {
-
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
