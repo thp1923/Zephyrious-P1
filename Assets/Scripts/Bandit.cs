fced560563636ab1fa309cbb54b3c9f28555b7aa
nvmq1011 <quannvmps38853@gmail.com>
2024-07-06 11:44:28 +0700

    enemy

--- Assets/Scripts/Bandit.cs
+++ Assets/Scripts/Bandit.cs
@@ -8,10 +8,15 @@ public class Bandit : MonoBehaviour
     public int maxHeath = 100;
     int currentHeath;
     public int DamgeEnemy = 10;
+    public Transform player;
+    bool isFlip = false;
+    Rigidbody2D rb;
+    public float distance = 10f;
     // Start is called before the first frame update
     void Start()
     {
         currentHeath = maxHeath;
+        rb = GetComponent<Rigidbody2D>();
     }
     
     public void TakeDamge(int damge)
@@ -43,6 +48,35 @@ public class Bandit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+    void Run()
+    {
+        if(Vector2.Distance(player.position, rb.position) < distance)
+        {
+            banditAim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            banditAim.SetBool("IsRunning", false);
+        }
+    }
+    public void Flip()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if(transform.position.x > player.position.x && isFlip)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlip = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlip)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlip = true;
+        }
     }
 }
--- Assets/Scripts/EnemyRun.cs
+++ Assets/Scripts/EnemyRun.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class EnemyRun : StateMachineBehaviour
 {
     public float speed = 2.5f;
-
+    
     Transform player;
     Rigidbody2D rb;
+    Vector2 target;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
+        
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        animator.GetComponent<Bandit>().Flip();
+        target = new Vector2(player.position.x, rb.position.y);
         Vector2 nowPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(nowPos);
     }
