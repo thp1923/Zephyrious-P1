6203f708101a730353cce1e5e4168f4730b0d8db
Tran Hoang Phuc <99522520+thp1923@users.noreply.github.com>
2024-07-22 17:16:55 +0700

    Cutscene test

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneCutScene : MonoBehaviour
+{
+    public string SceneName;
+    public Collider2D CutsceneCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    void Update()
+    {
+        Cutscene();
+    }
+    void Cutscene()
+    {
+        if (CutsceneCol.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            SceneManager.LoadScene(SceneName);
+            Debug.Log("Player is touching Cutscene collider");
+        }
+    }
+}
