17a4f8b3a5e0f035c0a206f40cf0a5b8085073cd
Tran Hoang Phuc <99522520+thp1923@users.noreply.github.com>
2024-07-28 19:59:23 +0700

    Fix

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public string Scenename;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,7 @@ public class MenuController : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene(Scenename);
     }
     public void QuitGame()
     {
